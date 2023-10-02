bbb759e49c14e879d3625e969f10c517e293af21
Johansz <johansz2030@gmail.com>
2023-10-02 10:43:09 -0400

    Modif Ej05 del M03

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -77,19 +77,18 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-   if(letra === 'a'){
-     console.log('Es vocal');
-   } else if(letra === 'e'){
-       console.log('Es vocal');
-   }else if(letra === 'i'){
-       console.log('Es vocal');
-   }else if(letra === 'o'){
-       console.log('Es vocal');
-   }else if(letra === 'u'){
-       console.log('Es vocal');
-   } else{
-       console.log('Dato incorrecto');
+   function esVocal(letra) {
+      // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+      // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
+      // Si no es vocal, tambien debe retornar "Dato incorrecto".
+      // Tu código:
+      if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
+        return 'Es vocal';
+      } else{
+         return 'Dato incorrecto';
+      }
    }
+   esVocal('a');
 }
 esVocal('a');
 
